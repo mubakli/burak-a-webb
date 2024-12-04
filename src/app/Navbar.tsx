2d40324da7010e32e11cb8e65498128d587cba7c
Burak Asarcıklı <brkasarcikli@outlook.com>
2024-12-04 20:58:39 +0300

    trying to fix <a> link error.

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,31 +10,31 @@ export default function Navbar() {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <a href="/" className="text-2xl font-bold text-white">
+            <Link href="#home" className="text-2xl font-bold text-white">
               BuW
-            </a>
+            </Link>
           </div>
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex space-x-4">
-            <a
+            <Link
               href="#about"
               className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
             >
               About
-            </a>
-            <a
+            </Link>
+            <Link
               href="#services"
               className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
             >
               Projects
-            </a>
-            <a
+            </Link>
+            <Link
               href="#contact"
               className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
             >
               Contact
-            </a>
+            </Link>
           </div>
 
           {/* Mobile Menu Button */}
@@ -74,24 +75,24 @@ export default function Navbar() {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a
+            <Link
               href="#about"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
             >
               About
-            </a>
-            <a
+            </Link>
+            <Link
               href="#services"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
             >
               Projects
-            </a>
-            <a
+            </Link>
+            <Link
               href="#contact"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
             >
               Contact
-            </a>
+            </Link>
           </div>
         </div>
       )}
