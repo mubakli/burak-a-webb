e8285329ef1cf5471587e230aa47de971283ddb9
Burak Asarcıklı <brkasarcikli@outlook.com>
2024-12-17 18:39:58 +0300

    quick fix.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,19 +11,19 @@ export default function about() {
         className="rounded-lg" // Optional Tailwind class for styling
       />
       <p className="mt-20 mx-20 text-xl">
-        Hi there! I'm a second-year Computer Engineering student at Istanbul
-        Medeniyet University with a strong passion for web development.
-        Currently, I'm focused on advancing my skills in JavaScript, Next.js,
-        React.js, and Node.js, while also exploring mobile development,
+        Hi there! I&apos;m a second-year Computer Engineering student at
+        Istanbul Medeniyet University with a strong passion for web development.
+        Currently, I&apos;m focused on advancing my skills in JavaScript,
+        Next.js, React.js, and Node.js, while also exploring mobile development,
         blockchain, and AI projects. I believe in hands-on learning and prefer
-        building projects to watching tutorials. As a member of my university's
-        tech club, MedeniyeTekno, I've had the chance to contribute to various
-        tech-related initiatives, including developing a website for the club.
-        This experience has been instrumental in refining my web development
-        skills, from both the front-end to the back-end. I'm also building a
-        personal website to showcase my work and am always excited about taking
-        on new challenges that help me grow as a developer. Feel free to explore
-        my work and connect with me!
+        building projects to watching tutorials. As a member of my
+        university&apos;s tech club, MedeniyeTekno, I&apos;ve had the chance to
+        contribute to various tech-related initiatives, including developing a
+        website for the club. This experience has been instrumental in refining
+        my web development skills, from both the front-end to the back-end.
+        I&apos;m also building a personal website to showcase my work and am
+        always excited about taking on new challenges that help me grow as a
+        developer. Feel free to explore my work and connect with me!
       </p>
     </div>
   );
--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -4,16 +4,16 @@ export default function trade() {
       <p className="text-bold mb-10">Coming Soon: Virtual Trade Page 🚀</p>
       <p>
         This page will provide an interactive and risk-free platform for users
-        to experience virtual trading. Whether you're learning how trading works
-        or just testing strategies, you'll have a virtual balance to practice
-        buying and selling items. Stay tuned as we work to bring you features
-        like:
+        to experience virtual trading. Whether you&apos;re learning how trading
+        works or just testing strategies, you&apos;ll have a virtual balance to
+        practice buying and selling items. Stay tuned as we work to bring you
+        features like:
       </p>
       <li>Virtual balance to simulate real trades</li>
       <li>Easy-to-use interface for buying and selling items</li>
       <li>Trade history and performance tracking</li>
       <p className="mt-10">
-        Get ready to trade, learn, and have fun—all virtually! 🎉"
+        Get ready to trade, learn, and have fun—all virtually! 🎉
       </p>
     </div>
   );
